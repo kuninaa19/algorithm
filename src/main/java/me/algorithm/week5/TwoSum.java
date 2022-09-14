fcc2424b69a60d88843030e265dbbd7648b2c5f8
kuninaa19 <minchan0527@naver.com>
2022-09-14 18:01:07 +0900

    Code Refactoring - Two Sum

--- src/main/java/me/algorithm/week5/TwoSum.java
+++ src/main/java/me/algorithm/week5/TwoSum.java
@@ -3,6 +3,21 @@ package me.algorithm.week5;
 import java.util.HashMap;
 
 public class TwoSum {
+    public static int[] TwoSumRefactored(int[] nums, int target) {
+        HashMap<Integer, Integer> needNums = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int needNum = target - nums[i];
+
+            Integer needNumIdx = needNums.get(needNum);
+            if (needNumIdx != null) {
+                return new int[]{needNumIdx, i};
+            }
+            needNums.put(nums[i], i);
+        }
+        return null;
+    }
+
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
 
