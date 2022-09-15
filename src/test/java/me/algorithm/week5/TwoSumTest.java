2f2c3061336c178a94ce8a734fc7ab99a27c4aa3
kuninaa19 <minchan0527@naver.com>
2022-09-15 23:33:15 +0900

    코드 정렬 및 메서드 부분 수정

--- src/test/java/me/algorithm/week5/TwoSumTest.java
+++ src/test/java/me/algorithm/week5/TwoSumTest.java
@@ -2,6 +2,7 @@ package me.algorithm.week5;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TwoSumTest {
@@ -10,18 +11,18 @@ public class TwoSumTest {
     void testLengthTwo() {
         int[] nums = new int[]{2, 1};
         int target = 3;
-        int[] expected = new int[]{0,1};
+        int[] expected = new int[]{0, 1};
         int[] result = TwoSum.twoSum(nums, target);
-        assertArrayEquals(result, expected);
+        assertArrayEquals(expected, result);
     }
 
     @Test
     @DisplayName("배열이 5개이고 배열의 합이 타겟값과 동일")
     void testLengthFive() {
-        int[] nums = new int[]{2,4,1,5,9};
+        int[] nums = new int[]{2, 4, 1, 5, 9};
         int target = 9;
-        int[] expected = new int[]{1,3};
+        int[] expected = new int[]{1, 3};
         int[] result = TwoSum.twoSum(nums, target);
-        assertArrayEquals(result, expected);
+        assertArrayEquals(expected, result);
     }
 }
